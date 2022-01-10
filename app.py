44ac073c5befcc23f5cdce4c28390d067289c018
joeck <joelplambeck@gmail.com>
2022-01-10 16:30:36 +0100

    preparar los datos para el entranamiento

--- app.py
+++ app.py
@@ -4,6 +4,23 @@ from tkinter import filedialog
 from tkinter import messagebox
 import os
 
+from nltk.tokenize import RegexpTokenizer
+from nltk import WordNetLemmatizer
+
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegression
+
+import pandas as pd
+
+# Logica
+class Articulo:
+  def __init__(self, n, p, k):
+    self.file_name= n
+    self.path = p
+    self.keywords = k
+
 root = Tk()
 
 tabs = ttk.Notebook(root)
@@ -21,8 +38,71 @@ def getNoOdioDirectory():
     no_odio_input.delete(0, 'end')
     no_odio_input.insert(0, filedialog.askdirectory(title="Elige carpeta No Odio"))
 
+def tokenize(x):
+    return RegexpTokenizer(r'\w+').tokenize(x.lower())
+
+def lemmatize(x):
+ lemmatizer = WordNetLemmatizer()
+ return ' '.join([lemmatizer.lemmatize(word) for word in x])
+
+punctuation_signs = list("?:!.,;")
+stop_words = list()
+def create_features_from_text(text):
+    
+    # Dataframe creation
+    lemmatized_text_list = []
+    df = pd.DataFrame(columns=['Content'])
+    df.loc[0] = text
+    df['Content_Parsed_1'] = df['Content'].str.replace("\r", " ")
+    df['Content_Parsed_1'] = df['Content_Parsed_1'].str.replace("\n", " ")
+    df['Content_Parsed_1'] = df['Content_Parsed_1'].str.replace("    ", " ")
+    df['Content_Parsed_1'] = df['Content_Parsed_1'].str.replace('"', '')
+    df['Content_Parsed_2'] = df['Content_Parsed_1'].str.lower()
+    df['Content_Parsed_3'] = df['Content_Parsed_2']
+    for punct_sign in punctuation_signs:
+        df['Content_Parsed_3'] = df['Content_Parsed_3'].str.replace(punct_sign, '')
+    df['Content_Parsed_4'] = df['Content_Parsed_3'].str.replace("'s", "")
+    wordnet_lemmatizer = WordNetLemmatizer()
+    lemmatized_list = []
+    text = df.loc[0]['Content_Parsed_4']
+    text_words = text.split(" ")
+    for word in text_words:
+        lemmatized_list.append(wordnet_lemmatizer.lemmatize(word, pos="v"))
+    lemmatized_text = " ".join(lemmatized_list)    
+    lemmatized_text_list.append(lemmatized_text)
+    df['Content_Parsed_5'] = lemmatized_text_list
+    df['Content_Parsed_6'] = df['Content_Parsed_5']
+    for stop_word in stop_words:
+        regex_stopword = r"\b" + stop_word + r"\b"
+        df['Content_Parsed_6'] = df['Content_Parsed_6'].str.replace(regex_stopword, '')
+    df = df['Content_Parsed_6']
+    df = df.rename(columns={'Content_Parsed_6': 'Content_Parsed'})
+    
+    # TF-IDF
+    features = tfidf.transform(df).toarray()
+    
+    return features
+
+def generateDF(path):
+    print(path)
+    df = pd.DataFrame({"name": [], "content":[]})
+    files = os.listdir(path)
+    print(files)
+    for file in files:
+        file_path = os.path.join(path, file)
+        print(file_path)
+        if os.path.isfile(file_path):
+            with open(file_path, 'r', encoding="ISO-8859-1") as f:
+                df = df.append({"name": file, "content": f.read()}, ignore_index=True)
+    return df
+
+
 def train():
-    pass
+    df = generateDF(odio_input.get())
+    # stopwords
+    df['tokens'] = df['content'].map(tokenize)
+    df['lemma'] = df['tokens'].map(lemmatize)
+    print(df.head())
 
 odio_files = []
 no_odio_files = []
