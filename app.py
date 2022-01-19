4e4f9c4483f8feca7b7ef6ff104ef0611c3e234a
joeck <joelplambeck@gmail.com>
2022-01-19 16:29:08 +0100

    performance

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import Text
 from tkinter import filedialog
 from tkinter import messagebox
-import os, re
+import os, pickle
 
 import nltk
 nltk.download("wordnet")
@@ -14,6 +15,7 @@ from nltk.stem.snowball import SnowballStemmer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.metrics import roc_curve, auc
 
@@ -34,6 +36,7 @@ trainFrame = ttk.Frame(tabs)
 clasiFrame = ttk.Frame(tabs)
 tabs.add(trainFrame, text="Entrenamiento")
 tabs.add(clasiFrame, text="Clasificacion")
+clf = ""
 
 def getOdioDirectory():
     odio_input.delete(0, 'end')
@@ -70,37 +73,51 @@ def generateDF(path, label):
                 df = df.append({"name": file, "odio": label, "content": f.read()}, ignore_index=True)
     return df
 
+def decisionTree(X_train_vector, X_test_vector,y_train, y_test):
+    print("############ decisionTree ############")
+    global clf
+    clf = DecisionTreeClassifier()
+    clf.fit(X_train_vector,y_train)
+    y_pred = clf.predict(X_test_vector)
+    print(confusion_matrix(y_test, y_pred))
+    print(classification_report(y_test, y_pred))
+    performanceText.insert("1.0", classification_report(y_test, y_pred))
+
+def logisticRegression(X_train_vector, X_test_vector,y_train, y_test):
+    print("############ logisticRegression ############")
+    global clf
+    clf=LogisticRegression(solver = 'liblinear', C=10, penalty = 'l2')
+    clf.fit(X_train_vector, y_train)
+    y_pred = clf.predict(X_test_vector)
+    print(confusion_matrix(y_test, y_pred))
+    print(classification_report(y_test, y_pred))
+    performanceText.insert("1.0", classification_report(y_test, y_pred))
+
+def split_transform(x, y):
+    X_train, X_test, y_train, y_test = train_test_split(x,y,test_size=0.2,shuffle=True)
+    tfidf_vectorizer = TfidfVectorizer(use_idf=True)
+    X_train_vectors_tfidf = tfidf_vectorizer.fit_transform(X_train) 
+    X_test_vectors_tfidf = tfidf_vectorizer.transform(X_test)
+    return [X_train_vectors_tfidf, X_test_vectors_tfidf, y_train, y_test]
 
 def train():
     try:
         df = pd.DataFrame({"name": [], "odio": [], "content":[]})
         df = df.append(generateDF(odio_input.get(), 1))
         df = df.append(generateDF(no_odio_input.get(), 0))
-        print(df.head())
+        # print(df.head())
         df['tokens'] = df['content'].map(tokenize)
-        print(df.head())
+        # print(df.head())
         df['tokens'] = df['tokens'].map(removeStopwords)
-        print(df.head())
+        # print(df.head())
         df['lemma'] = df['tokens'].map(stemming)
-        print(df.head())
-        print(df['lemma'])
+        # print(df.head())
+        # print(df['lemma'])
 
-        X_train, X_test, y_train, y_test = train_test_split(df['lemma'],df['odio'],test_size=0.2,shuffle=True)
-        tfidf_vectorizer = TfidfVectorizer(use_idf=True)
-        X_train_vectors_tfidf = tfidf_vectorizer.fit_transform(X_train) 
-        X_test_vectors_tfidf = tfidf_vectorizer.transform(X_test)
+        X_train_vectors_tfidf, X_test_vectors_tfidf, y_train, y_test = split_transform(df['lemma'],df['odio'])
 
-        lr_tfidf=LogisticRegression(solver = 'liblinear', C=10, penalty = 'l2')
-        lr_tfidf.fit(X_train_vectors_tfidf, y_train)
-
-        y_predict = lr_tfidf.predict(X_test_vectors_tfidf)
-        y_prob = lr_tfidf.predict_proba(X_test_vectors_tfidf)[:,1]
-        print(classification_report(y_test,y_predict))
-        print('Confusion Matrix:',confusion_matrix(y_test, y_predict))
-        
-        fpr, tpr, thresholds = roc_curve(y_test, y_prob)
-        roc_auc = auc(fpr, tpr)
-        print('AUC:', roc_auc)
+        decisionTree(X_train_vectors_tfidf, X_test_vectors_tfidf, y_train, y_test)
+        logisticRegression(X_train_vectors_tfidf, X_test_vectors_tfidf, y_train, y_test)
 
     except FileNotFoundError:
         messagebox.showwarning("At least one of the paths you provided isn't valid")
@@ -130,6 +147,10 @@ def updateSummaryFiles(var, index, mode):
 def updateSummaryAlgorithm(var, index, mode):
     algorithm_selected.config(text = algorithm.get())
 
+def saveModel():
+    #filename = 'finalized_model.sav'
+    #pickle.dump(model, open(filename, 'wb'))
+    pass
 
 # Odio Label
 ttk.Label(trainFrame, text="Noticias de Odio:").grid(column=0, row=0, sticky=W)
@@ -137,16 +158,18 @@ ttk.Label(trainFrame, text="Noticias de Odio:").grid(column=0, row=0, sticky=W)
 odio_path = StringVar()
 odio_path.trace_add("write", updateSummaryFiles)
 odio_input = ttk.Entry(trainFrame, textvariable=odio_path)
+#odio_input.insert(0, "/Users/joelplambeck/Documents/ZHAW/5_Semester/Proyecto Computacion/Plambeck_Joel.PC1.A3/RapidMiner/Odio")
 odio_input.grid(column=1, row=0, columnspan=2, sticky=(W, E))
 # Odio Button
 ttk.Button(trainFrame, text="Abrir", command=getOdioDirectory).grid(column=3, row=0, sticky=E)
 
 # No Odio Label
 ttk.Label(trainFrame, text="Noticias de No Odio:").grid(column=0, row=1, sticky=W)
-# Odio path input
+# No Odio path input
 no_odio_path = StringVar()
 no_odio_path.trace_add("write", updateSummaryFiles)
 no_odio_input = ttk.Entry(trainFrame, textvariable=no_odio_path)
+#no_odio_input.insert(0, "/Users/joelplambeck/Documents/ZHAW/5_Semester/Proyecto Computacion/Plambeck_Joel.PC1.A3/RapidMiner/No odio")
 no_odio_input.grid(column=1, row=1, columnspan=2, sticky=(W, E))
 # Odio Button
 ttk.Button(trainFrame, text="Abrir", command=getNoOdioDirectory).grid(column=3, row=1, sticky=E)
@@ -166,6 +189,7 @@ ttk.Button(trainFrame, text="Execute", default="active", command=train).grid(col
 for child in trainFrame.winfo_children(): 
     child.grid_configure(padx=5, pady=5)
 
+# Summary
 ttk.Label(trainFrame, text="Ejemplares de Odio:").grid(column=0, row=4, sticky=W)
 ttk.Label(trainFrame, text="Ejemplares de No Odio:").grid(column=0, row=5, sticky=W)
 ttk.Label(trainFrame, text="Total:").grid(column=0, row=6, sticky=W)
@@ -179,6 +203,11 @@ articles_total = ttk.Label(trainFrame, text="-")
 articles_total.grid(column=1, row=6, sticky=W)
 algorithm_selected = ttk.Label(trainFrame, text="-")
 algorithm_selected.grid(column=1, row=7, sticky=W)
+
+# Performance
+ttk.Label(trainFrame, text="Performance:").grid(column=0, row=9, sticky=W)
+performanceText = Text(trainFrame, height=8)
+performanceText.grid(column=1, row=9, sticky=W)
 
 # growing
 trainFrame.columnconfigure(0, weight=1, minsize=150)
@@ -222,4 +251,6 @@ root.geometry("800x400")
 root.title("Python classificador")
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
+no_odio_input.insert(0, "/Users/joelplambeck/Documents/ZHAW/5_Semester/Proyecto Computacion/Plambeck_Joel.PC1.A3/RapidMiner/No odio")
+odio_input.insert(0, "/Users/joelplambeck/Documents/ZHAW/5_Semester/Proyecto Computacion/Plambeck_Joel.PC1.A3/RapidMiner/Odio")
 root.mainloop()
